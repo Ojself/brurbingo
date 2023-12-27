a48367bc6707fffe8ca490e1d5a34a9c6f0b9c70
Tormod <tormod.flesjo@gmail.com>
2023-12-27 17:16:48 +0100

    quick commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     }, 80);
   };
   const rotation = horsePositionX % 15;
-  const horsePositionY = horsePositionX < -8 && horsePositionX > -160 ? -17 : 0;
+  const horsePositionY = horsePositionX < -8 && horsePositionX > -165 ? -17 : 0;
 
   return (
     <footer className='relative h-24 border-t-2 w-screen bg-yellow-100 flex items-center justify-center'>
